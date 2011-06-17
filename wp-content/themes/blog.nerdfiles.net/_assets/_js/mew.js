aafbca95268ba3d885e4c8a53cd4d6112b3450ac
Aaron Alexander <nerdfiles@gmail.com>
2011-06-17 00:41:06 -0500

    cleanup

--- wp-content/themes/blog.nerdfiles.net/_assets/_js/mew.js
+++ wp-content/themes/blog.nerdfiles.net/_assets/_js/mew.js
@@ -15,5 +15,30 @@ $(document).ready(function() {
     $Lout.text("an exodus");
     $Lin.text("transpierce");
     */
+    
+    $('p,ul,ol').hover(function() {
+    
+        var $self = $(this);
+        
+        console.log($self);
+        
+        $self.closest('div').find('p,ul,ol').delay(700).addClass('not-this');
+        
+        
+        $self.prev().delay(700).addClass('near-this');
+        $self.next().delay(700).addClass('near-this');
+        
+        $self.prev().prev().delay(700).addClass('near-this');
+        $self.next().next().delay(700).addClass('near-this');
+        
+        $self.delay(750).addClass('this');
+        
+    }, function() {
+    
+        var $self = $(this);
+        
+        $self.closest('div').find('p,ul,ol').delay(2000).removeClass('not-this').removeClass('this');
+    
+    });
 
 });
--- wp-content/themes/blog.nerdfiles.net/page.php
+++ wp-content/themes/blog.nerdfiles.net/page.php
@@ -20,20 +20,21 @@
 					    <div class="entry-meta-inner">
 						<span class="meta-sep">&para;</span>
 						<span class="entry-date"><?php _e('Posted', 'blogtxt') ?> <abbr class="published" title="<?php the_time('Y-m-d\TH:i:sO'); ?>"><?php unset($previousday); printf(__('%1$s', 'blogtxt'), the_date('d F Y', false)) ?></abbr></span>
-						<?php blogtxt_author_link(); // Function for author link option ?>
-						<span class="meta-sep">&sect;</span>
-						<span class="entry-category"><?php the_category(' &sect; ') ?></span>
-						<!--span class="meta-sep">&Dagger;</span-->
-						<!--span class="entry-comments"><?php comments_popup_link(__('Comments (0)', 'blogtxt'), __('Comments (1)', 'blogtxt'), __('Comments (%)', 'blogtxt')) ?></span-->
-						<!--span class="meta-sep">&deg;</span>
-						<span class="entry-tags"><?php the_tags(__('Tagged: ', 'blogtxt'), ", ", "") ?></span-->
-<?php edit_post_link(__('Edit', 'blogtxt'), "\t\t\t\t\t<span class=\"meta-sep\">&equiv;</span>\n\t\t\t\t\t<span class='entry-edit'>", "</span>\n"); ?>
+						<?php //blogtxt_author_link(); // Function for author link option ?>
+						<!--span class="meta-sep">&sect;</span-->
+						<!--span class="entry-category"><?php //the_category(' &sect; ') ?></span>
+						<span class="meta-sep">&Dagger;</span-->
+						<!--span class="entry-comments"><?php //comments_popup_link(__('Comments (0)', 'blogtxt'), __('Comments (1)', 'blogtxt'), __('Comments (%)', 'blogtxt')) ?></span-->
+						<!--span class="meta-sep">&deg;</span-->
+						<!--span class="entry-tags"><?php //the_tags(__('Tagged: ', 'blogtxt'), ", ", "") ?></span-->
+						<!--span class=\"meta-sep\">&equiv;</span-->
+<?php edit_post_link(__('Edit', 'blogtxt'), "\n\t\t\t\t\t<span class='entry-edit'>[", "]</span>\n"); ?>
                         </div>
 					</div>
 					
 				</div><!-- .post -->
 
-                <?php if ( get_post_custom_values('comments') ) comments_template() // Add a key/value of "comments" to load comments on a page ?>
+                <?php //if ( get_post_custom_values('comments') ) comments_template() // Add a key/value of "comments" to load comments on a page ?>
 
 			</div><!-- .hfeed -->
 		</div><!-- #content -->
