2ec6972298fd3cabc2a29ca6efae398f0531fb83
nerdfiles <nerdfiles@gmail.com>
2011-10-13 13:46:29 -0500

    cleanup merge

---
+++ wp-config.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * The base configurations of the WordPress.
+ *
+ * This file has the following configurations: MySQL settings, Table Prefix,
+ * Secret Keys, WordPress Language, and ABSPATH. You can find more information
+ * by visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
+ * wp-config.php} Codex page. You can get the MySQL settings from your web host.
+ *
+ * This file is used by the wp-config.php creation script during the
+ * installation. You don't have to use the web site, you can just copy this file
+ * to "wp-config.php" and fill in the values.
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'nerdfiles_webjou');
+
+/** MySQL database username */
+define('DB_USER', 'nerdfiles_webjou');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'f0xf0x0!6');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY', 'KIS6WxSBBnRPkBiUTdBv3nKhfLbY3t9wD8MxAA7hDVwkfbIimCGs6e3pIXUjEPoz');
+define('SECURE_AUTH_KEY', 'p6TflEXRAPlsaIqQhXm1nBqBZKILqXYXVxB7vgjzYUHS1INHGhnRvDQ7zw2nKEJ5');
+define('LOGGED_IN_KEY', 'GlbSkpxn8f9wj8saAuRjsbzLaESJBXtxsYknq8JVz5zdKj9QfoiGPVKEZYzFyfZO');
+define('NONCE_KEY', 'kFcxlyvS6VqqYiVg3pTbFQrX97Qiv0csCBay2fBt4wqjLXowayxg151Vpj7WuXUS');
+define('AUTH_SALT', 'uTnRPCZ5fLUpgAws1P1dD7xCKaI704Nvg0heHDIXukF5K1J3L49YgYsKAjc8jZnc');
+define('SECURE_AUTH_SALT', 'c0U5EhfUx0VtV3PWiG1wYpUXAYhxVOhHUznKvyh7G33Rlv5wX5h0r8ZYxgkVgBCZ');
+define('LOGGED_IN_SALT', 'gXz5WjWppwHKIA1oMOmNoR9ftSiBrkpEymzWulGHpLhtWPdSvz3RiVuGMPKuRdEL');
+define('NONCE_SALT', 'pFqu4AM1pkVIM4HbckB2rwKQs3oIjP78UBRPXmX40G3F2tMNIy3pAejasSC6Tt6L');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each a unique
+ * prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * WordPress Localized Language, defaults to English.
+ *
+ * Change this to localize WordPress. A corresponding MO file for the chosen
+ * language must be installed to wp-content/languages. For example, install
+ * de_DE.mo to wp-content/languages and set WPLANG to 'de_DE' to enable German
+ * language support.
+ */
+define('WPLANG', '');
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ */
+define('WP_DEBUG', false);
+
+/* That's all, stop editing! Happy blogging. */
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
--- wp-content/themes/blog.nerdfiles.net/archive.php
+++ wp-content/themes/blog.nerdfiles.net/archive.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 <?php get_header() ?>
 
@@ -58,65 +57,4 @@
 	</div><!-- #container -->
 
 <?php get_sidebar() ?>
-=======
-
-<?php get_header() ?>
-
-			<div class="hfeed">
-
-<?php the_post() ?>
-<?php if ( is_day() ) : ?>
-				<h2 class="page-title"><?php _e('Daily Archives', 'webjournal') ?> <span class="page-subtitle"><?php the_time(__('l, F Y', 'webjournal')) ?></span></h2>
-<?php elseif ( is_month() ) : ?>
-				<h2 class="page-title"><?php _e('Monthly Archives', 'webjournal') ?> <span class="page-subtitle"><?php the_time(__('F Y', 'webjournal')) ?></span></h2>
-<?php elseif ( is_year() ) : ?>
-				<h2 class="page-title"><?php _e('Yearly Archives', 'webjournal') ?> <span class="page-subtitle"><?php the_time(__('Y', 'webjournal')) ?></span></h2>
-<?php elseif ( is_author() ) : ?>
-				<h2 class="page-title"><?php _e('Author Archives', 'webjournal') ?> <span class="page-subtitle"><?php webjournal_author_hCard() ?></span></h2>
-				<div class="archive-meta"><?php if ( !(''== $authordata->user_description) ) : echo apply_filters('archive_meta', $authordata->user_description); endif; ?></div>
-<?php elseif ( is_category() ) : ?>
-				<h2 class="page-title"><?php _e('Category Archives:', 'webjournal') ?> <span class="page-subtitle"><?php echo single_cat_title(); ?></span></h2>
-<?php elseif ( is_tag() ) : ?>
-			<h2 class="page-title"><span class="archive-meta"><?php _e('Tag Archives:', 'webjournal') ?></span> <span class="page-subtitle"><?php single_tag_title(); ?></span></h2>
-<?php elseif ( isset($_GET['paged']) && !empty($_GET['paged']) ) : ?>
-				<h2 class="page-title"><?php _e('Archives', 'webjournal') ?> <?php printf(__('%1$s Archives', 'webjournal'), wp_specialchars(get_the_title(), 'double') ) ?></h2>
-<?php endif; ?>
-<?php rewind_posts() ?>
-
-<?php while ( have_posts() ) : the_post(); ?>
-
-				<div id="post-<?php the_ID() ?>" class="<?php webjournal_post_class() ?>">
-					<h3 class="entry-title"><a href="<?php the_permalink() ?>" title="<?php printf(__('Permalink to %s', 'webjournal'), wp_specialchars(get_the_title(), 1)) ?>" rel="bookmark"><?php the_title() ?></a></h3>
-					<div class="entry-content">
-<?php the_excerpt('<span class="more-link">'.__('Continue Reading &raquo;', 'webjournal').'</span>') ?>
-
-					</div>
-					<div class="entry-meta">
-						<span class="meta-sep">&para;</span>
-						<span class="entry-date"><?php _e('Posted', 'webjournal') ?> <abbr class="published" title="<?php the_time('Y-m-d\TH:i:sO'); ?>"><?php unset($previousday); printf(__('%1$s', 'webjournal'), the_date('d F Y', false)) ?></abbr></span>
-						<?php if ( !is_author() ) : webjournal_author_link(); endif; // Displays if NOT author archive page ?>
-						
-						<span class="meta-sep">&sect;</span>
-						<span class="entry-category"><?php if ( !is_category() ) { echo the_category(' &sect; '); } else { $other_cats = webjournal_other_cats(' &sect; '); echo $other_cats; } // Hides the current category if category archive ?></span>
-						<span class="meta-sep">&Dagger;</span>
-						<span class="entry-comments"><?php comments_popup_link(__('Comments (0)', 'webjournal'), __('Comments (1)', 'webjournal'), __('Comments (%)', 'webjournal')) ?></span>
-						<span class="meta-sep">&deg;</span>
-						<span class="tag-links"><?php if ( !is_tag() ) { echo the_tags(__('Tagged: ', 'webjournal'), ", ", ""); } else { $other_tags = webjournal_other_tags(', '); printf(__('Also tagged: %s', 'webjournal'), $other_tags); } ?></span>
-<?php edit_post_link(__('Edit', 'webjournal'), "\t\t\t\t\t<span class=\"meta-sep\">&equiv;</span>\n\t\t\t\t\t<span class='entry-edit'>", "</span>\n"); ?>
-					</div>
-				</div><!-- .post -->
-
-<?php endwhile ?>
-
-				<div id="nav-below" class="navigation">
-					<div class="nav-previous"><?php next_posts_link(__('&laquo; Earlier posts', 'webjournal')) ?></div>
-					<div class="nav-next"><?php previous_posts_link(__('Later posts &raquo;', 'webjournal')) ?></div>
-				</div>
-
-			</div><!-- .hfeed -->
-		</div><!-- #content -->
-	</div><!-- #container -->
-
-<?php get_sidebar() ?>
->>>>>>> a5048a728ddfa7a3f51c8dcc0d6ec4bb086793ff
 <?php get_footer() ?>
