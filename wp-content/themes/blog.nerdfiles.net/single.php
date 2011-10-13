e1e50a894d629a4a7b33b898036f25e10bc58e81
nerdfiles <nerdfiles@gmail.com>
2011-10-13 16:04:27 -0500

    single fix

--- wp-content/themes/blog.nerdfiles.net/single.php
+++ wp-content/themes/blog.nerdfiles.net/single.php
@@ -21,7 +21,7 @@
 					    <div class="entry-meta-inner">
     						<span class="meta-sep">&para;</span>
     						<span class="entry-date"><?php _e('Posted', 'webjournal') ?> <abbr class="published" title="<?php the_time('Y-m-d\TH:i:sO'); ?>"><?php unset($previousday); printf(__('%1$s', 'webjournal'), the_date('d F Y', false)) ?></abbr></span>
-    						<?php webjournal_author_link(); // Function for author link option ?>
+    						<?php webjournal_author_hCard(); // Function for author link option ?>
     						<span class="meta-sep">&sect;</span>
     						<span class="entry-category"><?php the_category(' &sect; ') ?></span>
     						<span class="meta-sep">&Dagger;</span>
