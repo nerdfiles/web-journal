43c160ec9d2d9228012a3f8702fedaad107f41f6
nerdfiles <nerdfiles@gmail.com>
2012-11-17 17:09:48 -0600

    cleanup

--- wp-content/themes/a-weedy-florilegium/sidebar.php
+++ wp-content/themes/a-weedy-florilegium/sidebar.php
@@ -11,6 +11,7 @@
 <div id="primary" class="sidebar">
   <ul>
     <?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar(1) ) : // Begin Widgets for Sidebar 1; displays widgets or default contents below ?>
+    <?php endif; ?>
   </ul>
 </div>
 
