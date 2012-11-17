6eddc0954e0bac34ff1f4ed5f51332c24a2f261e
nerdfiles <nerdfiles@gmail.com>
2012-11-17 12:57:14 -0600

    cleanup footer

--- wp-content/themes/a-weedy-florilegium/footer.php
+++ wp-content/themes/a-weedy-florilegium/footer.php
@@ -9,7 +9,7 @@
 
 <div id="footer">
 <div class="footer-inner">
-<p><a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/us/"><img src="http://i.creativecommons.org/l/by-sa/3.0/us/80x15.png"  alt="Creative Commons License" title="Attribution-ShareAlike 3.0 United States (CC BY-SA 3.0)" /></a> Chief Curator, nerdfiles (Aharon Alexander) | Back to <a href="#top">top</a></p>
+<p><a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/us/"><img src="http://i.creativecommons.org/l/by-sa/3.0/us/80x15.png"  alt="Creative Commons License" title="Attribution-ShareAlike 3.0 United States (CC BY-SA 3.0)" /></a> Chief Curator, <a href="http://nerdfiles.net">nerdfiles</a> (Aharon Alexander) | Back to <a href="#top">top</a></p>
 <div class="googleplus">
 
 <g:plusone></g:plusone>
