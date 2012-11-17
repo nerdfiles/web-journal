fc5c99e257a8a51073ae253ed559b22b188bcc93
nerdfiles <nerdfiles@gmail.com>
2012-11-17 12:58:19 -0600

    cleanup

--- wp-content/themes/a-weedy-florilegium/footer.php
+++ wp-content/themes/a-weedy-florilegium/footer.php
@@ -10,10 +10,6 @@
 <div id="footer">
 <div class="footer-inner">
 <p><a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/us/"><img src="http://i.creativecommons.org/l/by-sa/3.0/us/80x15.png"  alt="Creative Commons License" title="Attribution-ShareAlike 3.0 United States (CC BY-SA 3.0)" /></a> Chief Curator, <a href="http://nerdfiles.net">nerdfiles</a> (Aharon Alexander) | Back to <a href="#top">top</a></p>
-<div class="googleplus">
-
-<g:plusone></g:plusone>
-</div>
 <div class="twitter">
 <a href="http://twitter.com/filesofnerds" class="twitter-follow-button">Follow @filesofnerds</a>
 </div>
@@ -24,7 +20,6 @@
 
 <?php wp_footer() // Do not remove; helps plugins work ?>
 
-<script src="http://apis.google.com/js/plusone.js"></script>
 <script src="http://platform.twitter.com/widgets.js"></script>
 
 <!--script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/sopa/1.0/stopcensorship.js"></script-->
