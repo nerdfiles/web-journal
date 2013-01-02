bd7fb921f9182b645c09f4b1b3c2df3cc79557c4
nerdfiles <nerdfiles@gmail.com>
2013-01-01 22:53:28 -0800

    cleanup

--- wp-content/themes/a-weedy-florilegium/404.php
+++ wp-content/themes/a-weedy-florilegium/404.php
@@ -15,21 +15,21 @@
     <div id="post-0" class="post hentry p1">
 
       <h3 class="entry-title">
-        <?php _e('Nothing Found', 'webjournal') ?>
+        <?php _e('On the other hand', 'webjournal') ?>
       </h3>
 
       <div class="entry-content">
-        <p><?php _e('Sorry, but nothing matched your search criteria. Please try again with some different keywords.', 'webjournal') ?></p>
+        <p><?php _e('I have five fingers."', 'webjournal') ?></p>
       </div>
 
-      <form id="error404-searchform" method="get" action="<?php bloginfo('home') ?>">
+      <!--form id="error404-searchform" method="get" action="<?php bloginfo('home') ?>">
 
         <div>
           <input id="error404-s" name="s" type="text" value="<?php the_search_query() ?>" size="40" />
           <input id="error404-searchsubmit" name="searchsubmit" type="submit" value="<?php _e('Search', 'webjournal') ?>" />
         </div>
 
-      </form>
+      </form-->
 
     </div><!-- #post-0 .post -->
   </div><!-- .hfeed -->
