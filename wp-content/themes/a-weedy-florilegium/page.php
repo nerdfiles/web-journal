0e06a4461e6a2e189bdd059e48a125db7080f061
nerdfiles <nerdfiles@gmail.com>
2013-01-01 20:40:12 -0800

    cleanup

--- wp-content/themes/a-weedy-florilegium/page.php
+++ wp-content/themes/a-weedy-florilegium/page.php
@@ -9,35 +9,31 @@
 == -->
 
 
+  <div class="hfeed">
 
-<div class="hfeed">
+    <?php the_post() ?>
 
-<?php the_post() ?>
+    <div id="post-<?php the_ID(); ?>" class="<?php webjournal_post_class() ?>">
 
-<div id="post-<?php the_ID(); ?>" class="<?php webjournal_post_class() ?>">
+      <h1 class="entry-title"><?php the_title(); ?></h1>
 
-<h1 class="entry-title"><?php the_title(); ?></h1>
+      <?php if ( get_post_custom_values('authorlink') ) printf(__('<div class="archive-meta">By %1$s</div>', 'webjournal'), webjournal_author_link() ) // Add a key/value of "authorlink" to show an author byline on a page ?>
 
-<?php if ( get_post_custom_values('authorlink') ) printf(__('<div class="archive-meta">By %1$s</div>', 'webjournal'), webjournal_author_link() ) // Add a key/value of "authorlink" to show an author byline on a page ?>
+      <div class="entry-content">
+        <?php the_content() ?>
+      </div>
 
-<div class="entry-content">
+      <div class="entry-meta">
+        <div class="entry-meta-inner">
+        <span class="entry-date">Publish'd on <abbr class="published" title="<?php the_time('Y-m-d\TH:i:sO'); ?>"><i><?php unset($previousday); printf(__('%1$s', 'webjournal'), the_date('d F Y', false)) ?></i></abbr></span>
+        <?php edit_post_link(__('Edit this entry', 'webjournal'),'<div class="entry-edit">','</div>') ?>
+        </div>
+      </div>
 
-<?php the_content() ?>
+    </div><!-- .post -->
 
-</div>
+  </div><!-- .hfeed -->
 
-<div class="entry-meta">
-<div class="entry-meta-inner">
-<span class="entry-date">Last touch'd on <abbr class="published" title="<?php the_modified_time('Y-m-d\TH:i:sO'); ?>"><i><?php unset($previousday); printf(__('%1$s', 'webjournal'), the_modified_date('d F Y', false)) ?></i></abbr></span>
-<?php edit_post_link(__('Edit this entry', 'webjournal'),'<div class="entry-edit">','</div>') ?>
-</div>
-</div>
-
-</div><!-- .post -->
-
-<?php //comments_template() // Add a key/value of "comments" to load comments on a page ?>
-
-</div><!-- .hfeed -->
 </div><!-- #content -->
 </div><!-- #container -->
 
