4b6a6b7419f00c1c46521a2597272df145ff0e7f
nerdfiles <nerdfiles@gmail.com>
2013-08-16 18:38:05 -0500

    cleanup

--- wp-content/plugins/wordless/wordless/preprocessors.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once "process_builder.php";
-require_once "preprocessors/compass_preprocessor.php";
-require_once "preprocessors/sprockets_preprocessor.php";
--- wp-content/themes/wordless/tmp/234182efe41fce309b7c677fe26d3af7.php
+++
@@ -1,3 +0,0 @@
-<?php
-require_once '/home/nerdfiles/webapps/webjournal/wp-content/plugins/wordless/vendor/phamlp/haml/HamlHelpers.php';
-?>  <?php wp_footer(); ?>
